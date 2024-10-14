37f99d36e8327b9615c7b7d6e6b15a7b39a65add
m.kuzneсova <m.kuznecova.peppers.studio@gmail.com>
2024-10-14 11:52:35 +0300

    ver 0.011

---
+++ _next/static/VS6JniBUDeQ1D0uLOhFvW/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/136-322c9dcfbbdd140b.js","static/chunks/pages/index-d6bbe744e71a3b36.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
